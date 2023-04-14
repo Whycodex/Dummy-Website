a824dbea9be5aa9d2390bd4ce1e3f94b4587e09e
Whycodex <neerajgupta12300@gmail.com>
2023-04-15 02:43:14 +0530

    Changed styling back

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function App() {
   return (
-    <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: 120}}>
+    <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: 100}}>
       <button>Send Request</button>
       <div style={{padding: 100}}>Recieved data</div>
     </div>
