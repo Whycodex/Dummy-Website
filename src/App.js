0bab43fdcf846b6365212132d6d090f3a88f3dce
Whycodex <neerajgupta12300@gmail.com>
2023-04-15 16:36:34 +0530

    Changed content of the code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 
 function App() {
+  const sendDataToReactNativeApp = async () => {
+    window.ReactNativeWebView.postMessage('Data from WebView / Website');
+  };
   return (
-    <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: 100}}>
-      <button>Send Request</button>
-      <div style={{padding: 100}}>Recieved data</div>
-    </div>
+    <div>
+        html: ` 
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <button onclick="sendDataToReactNativeApp()" style={{padding: 20, width: 200, fontSize: 20, color: 'white', backgroundColor: '#6751ff'}}>
+          Send Data To React Native App
+        </button>
+        `,
+      </div>
   );
 }
 
