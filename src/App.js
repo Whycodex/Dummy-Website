16477483b067dd9b8cbd65926007e24dd4bf3120
Whycodex <neerajgupta12300@gmail.com>
2023-04-15 16:47:47 +0530

    Changed onclick function

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from "react";
 
 function App() {
-  const sendDataToReactNativeApp = async () => {
-    window.ReactNativeWebView.postMessage('Data from WebView / Website');
-  };
   return (
-    <div>
-        html: ` 
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <button onclick="sendDataToReactNativeApp()" style={{padding: 20, width: 200, fontSize: 20, color: 'white', backgroundColor: '#6751ff'}}>
-          Send Data To React Native App
-        </button>
-        `,
-      </div>
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <button
+        onclick={async () => {
+          window.ReactNativeWebView.postMessage('Data from WebView / Website');
+        }}
+        style={{
+          padding: 20,
+          width: 200,
+          fontSize: 20,
+          color: "white",
+          backgroundColor: "#6751ff"
+        }}
+      >
+        Send Data To React Native App
+      </button>
+    </>
   );
 }
 
